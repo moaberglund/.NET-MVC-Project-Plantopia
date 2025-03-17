cb87af85239ca74224c36f12c65b1d011ca05e7b
moaberglund <moa.21@hotmail.com>
2025-03-17 20:05:08 +0100

    Add CRUD views for NewsModel including Create, Edit, Delete, Details, and Index

---
+++ Controllers/NewsController.cs
@@ -0,0 +1,157 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Plantopia.Data;
+using Plantopia.Models;
+
+namespace Plantopia.Controllers
+{
+    public class NewsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public NewsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: News
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.News.ToListAsync());
+        }
+
+        // GET: News/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var newsModel = await _context.News
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (newsModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(newsModel);
+        }
+
+        // GET: News/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: News/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Content,ImageName,CreatedBy")] NewsModel newsModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(newsModel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(newsModel);
+        }
+
+        // GET: News/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var newsModel = await _context.News.FindAsync(id);
+            if (newsModel == null)
+            {
+                return NotFound();
+            }
+            return View(newsModel);
+        }
+
+        // POST: News/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Content,ImageName,CreatedBy")] NewsModel newsModel)
+        {
+            if (id != newsModel.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(newsModel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!NewsModelExists(newsModel.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(newsModel);
+        }
+
+        // GET: News/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var newsModel = await _context.News
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (newsModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(newsModel);
+        }
+
+        // POST: News/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var newsModel = await _context.News.FindAsync(id);
+            if (newsModel != null)
+            {
+                _context.News.Remove(newsModel);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool NewsModelExists(int id)
+        {
+            return _context.News.Any(e => e.Id == id);
+        }
+    }
+}
