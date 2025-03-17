654e1168de1fbf82b15f27862e1677f5a7ad9d4b
moaberglund <moa.21@hotmail.com>
2025-03-17 20:55:55 +0100

    Update NewsController to handle image file uploads during news creation

--- Controllers/NewsController.cs
+++ Controllers/NewsController.cs
@@ -13,10 +13,14 @@ namespace Plantopia.Controllers
     public class NewsController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly IWebHostEnvironment _hostEnvironment;
+        private readonly string wwwRootPath;
 
-        public NewsController(ApplicationDbContext context)
+        public NewsController(ApplicationDbContext context, IWebHostEnvironment hostEnvironment)
         {
             _context = context;
+            _hostEnvironment = hostEnvironment;
+            wwwRootPath = hostEnvironment.WebRootPath;
         }
 
         // GET: News
@@ -54,10 +58,30 @@ namespace Plantopia.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Title,Content,ImageName")] NewsModel newsModel)
+        public async Task<IActionResult> Create([Bind("Id,Title,Content,ImageFile")] NewsModel newsModel)
         {
             if (ModelState.IsValid)
             {
+
+                // check for image
+                if (newsModel.ImageFile != null)
+                {
+                    // Generate a unique filename
+                    string fileName = Path.GetFileNameWithoutExtension(newsModel.ImageFile.FileName);
+                    string extension = Path.GetExtension(newsModel.ImageFile.FileName);
+
+                    newsModel.ImageName = fileName = fileName.Replace(" ", String.Empty) + DateTime.Now.ToString("yymmssfff") + extension;
+                
+                    // Save the image to the wwwroot/images/news folder
+                    string path = Path.Combine(wwwRootPath + "/images", fileName);
+
+                    // Store in file system
+                    using (var fileStream = new FileStream(path, FileMode.Create))
+                    {
+                        await newsModel.ImageFile.CopyToAsync(fileStream);
+                    }
+                
+                }
                 _context.Add(newsModel);
 
                 // Add the current user as the creator of the news
